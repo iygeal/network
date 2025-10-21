b39058e3d65c97a9131f78340225c64b96666925
Innocent Anoziee <iygeal@gmail.com>
2025-10-21 08:05:13 +0100

    Created a Post model and set it to display newest post first, by setting the timestamp in descending order using Meta from Django ORM

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+
+from django.utils import timezone
+
+class Post(models.Model):
+    """
+    A post created by a user, containing content and a timestamp.
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.content[:30]}..."
+
+    class Meta:
+        ordering = ['-timestamp']
